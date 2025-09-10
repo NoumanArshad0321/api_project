d49e9fc5296d853b6e0cf1009087ace6ad11d804
NoumanArshad0321 <noumanarshad480@gmail.com>
2025-09-10 16:29:33 +0500

    CURD for function and class based view

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,7 @@ from . import views
 
 urlpatterns = [
     path('students/', views.studentsView),
+    path('students/<int:pk>/', views.studentsDetailView),
+    path('employees/', views.employeesView.as_view()),
+    path('employees/<int:pk>/', views.employeesDetail.as_view()),
 ]
